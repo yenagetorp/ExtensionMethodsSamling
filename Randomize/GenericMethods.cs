589508d33033a223347ce63774b070cc9b796b6a
Yen Agetorp <xyagetorp@stratiteq.com>
2019-01-29 16:56:38 +0100

    te

---
+++ Randomize/GenericMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Randomize
+{
+    static class GenericMethods
+    {
+        public static void Print<T>(this IEnumerable<T> collectionToPrint)
+        {
+            Print(collectionToPrint, a => a.ToString());
+        }
+
+        public static void Print<T>(this IEnumerable<T> collectionToPrint, Func<T, string> convertItemToString)
+        {
+            foreach (var item in collectionToPrint)
+            {
+                Console.WriteLine(convertItemToString(item));
+            }
+        }
+
+        public static void SortGenericArray<T>(this List<T> anyArr, Func<T, T, bool> lessThan)
+        {
+            for (int i = 0; i < anyArr.Count - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < anyArr.Count; j++)
+                    if (lessThan(anyArr[j], anyArr[minIndex]))
+                        minIndex = j;
+                T tmp = anyArr[i];
+                anyArr[i] = anyArr[minIndex];
+                anyArr[minIndex] = tmp;
+            }
+        }
+
+        public static void Randomize<T>(this T[] items)
+        {
+            Random rand = new Random();
+
+            // For each spot in the array, pick
+            // a random item to swap into that spot.
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                int j = rand.Next(i, items.Length);
+                T temp = items[i];
+                items[i] = items[j];
+                items[j] = temp;
+            }
+        }
+    }
+}
---
+++ Randomize/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Randomize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] allemployees = new string[] {"Aaron John", "Emilia Agetorp", "Elias Agetorp", "Linnea Scott", "Adam Theodor", "Benson Edward", "Benney Svensson", "Lisa Li", "Tony Ganegård", "SamuelAgetorp", "Yen Agetorp", "Chris Rufus", "Eva Francis", "Nina Kim"};
+            foreach (var employee in allemployees)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.WriteLine();
+            allemployees.Randomize();
+            foreach (var employee in allemployees)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.WriteLine();
+            int numOfgroups = 3;
+            int group_num = 0;
+            List<string> randomorderedEmployees = new List<string>();
+            for (int i = 0; i < allemployees.Length; i++)
+            {
+                randomorderedEmployees.Add($"{group_num}  {allemployees[i]}"); 
+                group_num = ++group_num % numOfgroups;
+                Console.WriteLine(randomorderedEmployees[i]);
+            }
+
+
+
+        }
+    }
+}
